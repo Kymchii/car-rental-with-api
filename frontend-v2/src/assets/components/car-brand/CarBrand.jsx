f7eba2fafcf64a7d227ae7ea96d9aa8ec5442103
Kymchii <wwwahyu.march@gmail.com>
2026-03-06 20:01:10 +0700

    update car brand

--- frontend-v2/src/assets/components/car-brand/CarBrand.jsx
+++ frontend-v2/src/assets/components/car-brand/CarBrand.jsx
@@ -1,6 +1,7 @@
 import { ButtonWhite } from "../ButtonWhite"
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+import { API_BASE_URL } from "../../../config/api"
 
 export const CarBrand = ({ carBrands, setSelectedBrand, selectedBrand, loading }) => {
     return (
@@ -15,7 +16,7 @@ export const CarBrand = ({ carBrands, setSelectedBrand, selectedBrand, loading }
             ) : (
                 carBrands.map((item) => (
                     <div key={item.id} className="relative h-24 bg-linear-to-b from-blue-500 via-blue-300 to-blue-100 shadow-sm p-6 rounded-md group">
-                        <img src={`http://localhost:8000/storage/${item.logo}`} alt={item.name} className="w-full h-full object-contain object-center" />
+                        <img src={`${API_BASE_URL}/storage/${item.logo}`} alt={item.name} className="w-full h-full object-contain object-center" />
                         <div className="absolute flex justify-center items-center inset-0 bg-slate-950/35 rounded-md opacity-0 group-hover:opacity-100 group-active:opacity-100 delay-75 transition-all">
                             <ButtonWhite onClick={() => setSelectedBrand(selectedBrand === item.id ? null : item.id)} >See car</ButtonWhite>
                         </div>
