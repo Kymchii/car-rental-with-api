6a3c8ff94407b6532437fb98580b7b70f2efe489
Kymchii <wwwahyu.march@gmail.com>
2026-03-06 19:43:52 +0700

    update api base url

--- frontend-v2/src/config/api.js
+++ frontend-v2/src/config/api.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000'
+// export const API_BASE_URL = 'http://127.0.0.1:8000' Local api
+
+// Public API
+export const API_BASE_URL = 'https://car-rental-backend-production-466f.up.railway.app/'
